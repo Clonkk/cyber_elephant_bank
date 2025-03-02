643f0c706853008cb83e429fc17142e33dbab44d
Clonkk <rf.clonk@linuxmail.org>
2025-03-02 15:51:18 +0100

    clean up, add comments

--- main.py
+++ main.py
@@ -1,208 +1,149 @@
-import os
-from functools import wraps
 from time import time
 
 import dash_bootstrap_components as dbc
 from dash import (
-    ctx,
     Dash,
     Input,
     Output,
     State,
+    ctx,
     dcc,
     html,
 )
 from dash_auth import BasicAuth
 
 # Read more about DBC
-# https://dash-bootstrap-components.opensource.faculty.ai/examples/
-# https://dash-bootstrap-components.opensource.faculty.ai/docs/
-# https://dash-bootstrap-components.opensource.faculty.ai/docs/components/
 from flask import Flask, request
 from tinydb import Query, TinyDB
-import argparse
 
 # Can download stylesheet from internet
-#  external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# Can use predefined dbc themes
-external_stylesheets = [dbc.themes.BOOTSTRAP]
-
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # Create Flask server
-server = Flask(__name__)  # necessary for video stream, which uses flask stream
 # Create Dash application and pass Flask server as argument
+server = Flask(__name__)  # necessary for video stream, which uses flask stream
 app = Dash(
     server=server,
     # Download a CSS style
-    external_stylesheets=external_stylesheets,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True,
 )
 
-# Prepare small db
 db = TinyDB("db.json")
 User = Query()
+
+# Initial name / password configuration
+# For login, uppercase matter
 USER_PWD = {
-    "sinistre": "123",
-    "prof": "123",
-    "admin": "admin",
+    "Sinistre": "123",
+    "Prof": "123",
+    "Pixie": "456",
+    "Maverick": "456",
 }
+
+
+def norm(username):
+    """
+    Normalise name to avoid upper case / lower case / space issues
+    """
+    return username.lower().strip()
+
+
+VALID_USERS = [norm(u) for u in USER_PWD.keys()]
 
 
 # Set default value
 def get_init_balance(username):
-    match username:
+    """
+    Initial value per user
+    """
+    match norm(username):
         case "sinistre":
             return 1500
 
         case "prof":
             return 700
 
-        case "admin":
-            return 10_000
+        case "pixie":
+            return 50_000
+
+        case "maverick":
+            return 50_000
 
 
 def is_valid_name(name):
-    if name in list(USER_PWD.keys()):
+    """
+    Check if name is a valid user
+    """
+    if norm(name) in VALID_USERS:
         return True
     return False
 
 
-def do_transfer(from_name, to, amount):
-    amount = int(amount)
-    print(f"begin do_transfer({from_name}, {to}, {amount})")
+def add(lhs, rhs):
+    return lhs + rhs
 
-    qry = User.name == from_name
-    if True:
-        res = db.search(qry)
-        balance = res[0]["balance"]
-        balance = balance - amount
 
-        history = res[0]["history"]
-        history.append({"from": from_name, "to": to, "amount": amount})
+def sub(lhs, rhs):
+    return lhs - rhs
 
-        output = {"balance": balance, "history": history}
-        db.update(output, qry)
 
-    qry = User.name == to
-    if True:
-        res = db.search(qry)
-        balance = res[0]["balance"]
-        balance = balance + amount
+def _exec_op(qry_name, from_name, to, amount, op):
+    """
+    Helper function to avoid repeating code during transaction
+    """
+    qry = User.name == qry_name
+    res = db.search(qry)
+    balance = res[0]["balance"]
+    balance = op(balance, amount)
 
-        history = res[0]["history"]
-        history.append({"from": from_name, "to": to, "amount": amount})
+    history = res[0]["history"]
+    history.append({"from": from_name, "to": to, "amount": amount})
 
-        output = {"balance": balance, "history": history}
-        db.update(output, qry)
+    output = {"balance": balance, "history": history}
+    db.update(output, qry)
 
+
+def update_global_history(from_name, to, amount):
+    """
+    The __history__ contains the history of all transaction (for easy access)
+    """
     qry = User.name == "__history__"
-    if True:
-        res = db.search(qry)
-        history = res[0]["history"]
-        history.append({"from": from_name, "to": to, "amount": amount})
-        db.update({"history": history}, qry)
-        print(history)
-
-    print("=== end do_transfer ===\n")
+    res = db.search(qry)
+    history = res[0]["history"]
+    history.append({"from": from_name, "to": to, "amount": amount})
+    db.update({"history": history}, qry)
 
 
-def init():
-    BasicAuth(
-        app,
-        USER_PWD,
-        secret_key="cyber_elephant",
-        # groups not in use
-        user_groups={"player": ["sinistre", "prof"], "pnj": ["admin"]},
-    )
-    db.insert({"name": "__history__", "balance": 0, "history": []})
-    for user in USER_PWD.keys():
-        amount = get_init_balance(user)
-        db.insert({"name": user, "balance": amount, "history": []})
+def do_transfer(from_name, to, amount):
+    """
+    Do a bank transfer
+    """
+    balance = get_current_balance(from_name)
+    if not is_valid_name(to):
+        return "Destinataire invalide"
+    if not isinstance(amount, int) or amount > balance:
+        return "Montant impossible. Vous n'avez pas assez d'argent."
+    amount = int(amount)
 
-    ret = dbc.Row(
-        [
-            dbc.Col(
-                [
-                    html.H6("Balance: ??????", id="balance"),
-                ]
-            ),
-            dbc.Col(
-                dbc.Row(
-                    [
-                        dcc.Input(
-                            id="transfer-id",
-                            placeholder="send to",
-                            type="text",
-                        ),
-                        dcc.Input(
-                            id="transfer-amount",
-                            min=0,
-                            step=1,
-                            placeholder=0.0,
-                            type="number",
-                        ),
-                    ]
-                )
-            ),
-            dbc.Col(
-                [dbc.Button("Transfer", color="success", id="do-transfer", href="/")]
-            ),
-        ]
-    )
+    print(f"begin do_transfer({from_name}, {to}, {amount})")
+    _exec_op(from_name, from_name, to, amount, sub)
+    _exec_op(to, from_name, to, amount, add)
+    update_global_history(from_name, to, amount)
+    return None
 
-    # Special case for admin
-    layout = [
-        html.H2("placeholder name", id="name"),
-        html.Hr(),
-        ret,
-        html.Hr(),
-        html.H2("History"),
-        html.Div(id="history_table"),
-    ]
 
-    app.layout = html.Div(
-        [
-            html.Div(id="output", children=layout),
-            dcc.Location(id="url", refresh=False),
-            dcc.Store(id="history", storage_type="local", data=[]),
-        ],
-        className="container",
-    )
-
-    @app.callback(
-        [
-            Output(component_id="name", component_property="children"),
-            Output(component_id="balance", component_property="children"),
-            Output(component_id="history_table", component_property="children"),
-        ],
-        [
-            Input(component_id="url", component_property="pathname"),
-            Input(component_id="do-transfer", component_property="n_clicks"),
-        ],
-        [
-            State(component_id="transfer-id", component_property="value"),
-            State(component_id="transfer-amount", component_property="value"),
-        ],
-        allow_duplicate=True,
-    )
-    def update_output_div(pathname, n_clicks, transfer_id, transfer_amount):
-        username = request.authorization["username"]
-        print(ctx.triggered_id)
-
-        last_trig = ctx.triggered_id
-        match last_trig:
-            case "url":
-                print("initial load")
-
-            case "do-transfer":
-                print(f"perform transfer({transfer_id}, {transfer_amount})")
-                if is_valid_name(transfer_id) and isinstance(transfer_amount, int):
-                    do_transfer(username, transfer_id, transfer_amount)
-
-        history_table, curr_balance = make_history_table(username)
-        return [username, curr_balance, history_table]
+def get_current_balance(username):
+    """
+    Return current balance
+    """
+    user = db.search(User.name == username)[0]
+    return int(user["balance"])
 
 
 def make_history_table(username):
+    """
+    Make the history table per user
+    """
     ts = time()
 
     table_header = [
@@ -219,31 +160,22 @@ def make_history_table(username):
     ]
 
     transactions = []
-    init_val = get_init_balance(username)
-    val = init_val
+    val = 0  # get_init_balance(username)
     history = db.search(User.name == username)[0]["history"]
     for row in history:
-        if row["from"] == "bank":
-            continue
-
         if username == row["from"]:
-            amount = float(row["amount"])
-            val = val - amount
-            curr_val = val
-            curr_row = list(row.values())
-            curr_row.append(curr_val)
+            val = val - int(row["amount"])
+            curr_row = [row["from"], row["to"], row["amount"], val]
             transactions.append(curr_row)
         elif username == row["to"]:
-            amount = float(row["amount"])
-            val = val + amount
-            curr_val = val
-            curr_row = list(row.values())
-            curr_row.append(curr_val)
+            val = val + int(row["amount"])
+            curr_row = [row["from"], row["to"], row["amount"], val]
             transactions.append(curr_row)
 
-    print(transactions)
-
     def make_line(t):
+        """
+        Helper function to format a transaction row in the history table
+        """
         from_name, to, amount, balance = t[0], t[1], t[2], t[3]
         if from_name == username:
             from_name = from_name + " (me)"
@@ -268,30 +200,142 @@ def make_history_table(username):
     return table, val
 
 
-parser = argparse.ArgumentParser(description="Bank for CyberElephant")
+### Init and reset databse
+def db_init():
+    """
+    Initialise database. Do nothing if databse already contains element
+    """
+    if len(db.all()) == 0:
+        db.insert({"name": "__history__", "balance": 0, "history": []})
+        db.insert({"name": "bank", "balance": 1e7, "history": []})
+        for user in VALID_USERS:
+            amount = get_init_balance(user)
+            db.insert({"name": user, "balance": 0, "history": []})
+            do_transfer("bank", user, amount)
 
 
-def main():
-    parser.add_argument(
-        "--clear",
-        type=bool,
-        help="This argument reset the existing database. Current data will be lost",
-    )
-    args = parser.parse_args()
-    print(args)
+def db_reset():
+    """
+    Reset database
+    """
+    db.truncate()
+    db_init()
 
-    if args.clear:
-        db.truncate()
-        init()
 
-        do_transfer("admin", "sinistre", 150)
-        do_transfer("prof", "sinistre", 300)
-        do_transfer("sinistre", "prof", 150)
+### Auth stuff ###
+BasicAuth(
+    app,
+    USER_PWD,
+    secret_key="cyber_elephant",
+    # groups not in use
+    user_groups={"player": ["sinistre", "prof"], "pnj": ["pixie, maverick"]},
+)
 
-    # print(db.all())
+### Layout section ###
+ret = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Balance: ??????", id="balance"),
+            ]
+        ),
+        dbc.Col(
+            dbc.Row(
+                [
+                    dcc.Input(
+                        id="transfer-id",
+                        placeholder="destinataire",
+                        type="text",
+                    ),
+                    dcc.Input(
+                        id="transfer-amount",
+                        min=0,
+                        step=1,
+                        placeholder=0.0,
+                        type="number",
+                    ),
+                ]
+            )
+        ),
+        dbc.Col([dbc.Button("Transfer", color="success", id="do-transfer", href="/")]),
+    ]
+)
 
-    app.run_server(host="192.168.1.130", port=36050, debug=True)
+# Special case for admin
+layout = [
+    html.H2("placeholder name", id="name"),
+    html.Hr(),
+    ret,
+    dbc.Alert(
+        "This is a danger alert. Scary!",
+        id="err-msg",
+        dismissable=False,
+        is_open=False,
+        color="danger",
+    ),
+    html.Hr(),
+    html.H2("History"),
+    html.Div(id="history_table"),
+]
 
+app.layout = html.Div(
+    [
+        html.Div(id="output", children=layout),
+        dcc.Location(id="url", refresh=False),
+    ],
+    className="container",
+)
+### End layout section ###
+
+
+### Callback - behavior section ###
+@app.callback(
+    [
+        Output(component_id="name", component_property="children"),
+        Output(component_id="balance", component_property="children"),
+        Output(component_id="history_table", component_property="children"),
+        Output(component_id="err-msg", component_property="children"),
+        Output(component_id="err-msg", component_property="is_open"),
+    ],
+    [
+        Input(component_id="url", component_property="pathname"),
+        Input(component_id="do-transfer", component_property="n_clicks"),
+    ],
+    [
+        State(component_id="transfer-id", component_property="value"),
+        State(component_id="transfer-amount", component_property="value"),
+    ],
+    allow_duplicate=True,
+)
+def update_output_div(pathname, n_clicks, transfer_id, transfer_amount):
+    """
+    Trigger when page load or when the transfer button is clicked.
+    Return the update component to display.
+    """
+    username = request.authorization["username"]
+    username = norm(username)
+    err_msg = ""
+    err_msg_open = False
+    match ctx.triggered_id:
+        case "url":
+            pass
+
+        case "do-transfer":
+            print(f"perform transfer({transfer_id}, {transfer_amount})")
+            err_msg = do_transfer(username, norm(transfer_id), transfer_amount)
+            if err_msg:
+                err_msg_open = True
+
+    history_table, curr_balance = make_history_table(username)
+    balance = get_current_balance(username)
+    # Check history and amount are coherent, if not use stored value
+    if curr_balance != balance:
+        curr_balance = balance
+    return [username, curr_balance, history_table, err_msg, err_msg_open]
+
+
+### End allback section ###
 
 if __name__ == "__main__":
-    main()
+    db_init()
+    app.run_server(host="192.168.1.130", port=36050, debug=True)
